4ac48ab3fdd6b1fd21133590828212df776d125a
gloria lubwimi <gloria_lubwimi@hotmail.com>
2024-08-04 13:45:49 +0200

    created patmos.jpg and patmos2.jpg in frontend assets

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -9,7 +9,22 @@ const Dashboard = () => {
 
     return (
         <div className='dashboard'>
-            <h1>Welcome </h1>
+           <div className='col-1'>
+            <div className='bg-1'>
+                <h1>The perfect place to get married</h1>
+                <h2>In addition, when you book your have access to our side room.</h2>
+                <h2>It can accommodate 200 people</h2>
+                <h1 className='booking-title-1'>Book It Now</h1>
+            </div>
+           </div>
+           <div className='col-2'>
+            <div className='bg-2'>
+                <h1 className='booking-title-2'> The place for your celebration! </h1>
+                <h2>Her you can celebratte your birthday with a beautifule view.</h2>
+                <h2>It can accommodate 50 people</h2>
+                <h1 className='booking-title-1'>Book It Now</h1>
+            </div>
+           </div>
         </div>
     )
 }
