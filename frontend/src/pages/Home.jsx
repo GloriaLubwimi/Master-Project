5aa672fc98f5b32b5c151aa736e2463ff8675cc4
gloria lubwimi <gloria_lubwimi@hotmail.com>
2024-08-11 20:01:31 +0200

    added home page vide

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import homeVideo from '../assets/homeVideo.mp4'
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Home = () => {
 
@@ -7,16 +8,21 @@ const Home = () => {
     return (
         <div className='home'>
             
-            <div className='patmos4-img'>
+            <div className='video-img'>
+                <video src={homeVideo} autoPlay loop muted />
+                <div className='video-content'>
+                 <h1 style={{wordSpacing: '1rem'}}><span style={{fontWeight: 600, fontSize: '4rem'}}>WELCOME TO PATMOS</span><span style={{fontWeight:300, fontSize: '4rem'}}> CHURCH </span>  </h1>
+                 <button style={{height: '1.8rem', width: '15rem', borderRadius: '2rem', fontSize: '1rem', fontFamily: 'Georgia', wordSpacing: '0.2rem'}}> WATCH SERVICE ONLINE <FaArrowRightLong /> </button>
+                </div>
                 
             </div>
 
             <div className='home-description'>
-                <h1>Welcome to patmos church</h1>
+                <h1> Welcome to patmos church </h1>
             </div>
 
         </div>
     )
-}
+} 
 
 export default Home
