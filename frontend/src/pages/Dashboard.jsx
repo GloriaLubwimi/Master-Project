fa5cc57cd7a7726c14d6efb69111a0512ce94a03
gloria lubwimi <gloria_lubwimi@hotmail.com>
2024-08-24 15:48:43 +0200

    done with everiting

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -12,17 +12,17 @@ const Dashboard = () => {
         <div className='dashboard'>
            <div className='col-1'>
             <div className='bg-1'>
-                <h1>The perfect place to get married</h1>
-                <h2>In addition, when you book your have access to our side room.</h2>
-                <h2>It can accommodate 200 people</h2>
+                <h1 className='testing-1'>The perfect place to get married</h1>
+                <h2 className='testing-1'>In addition, when you book your have access to our side room.</h2>
+                <h2 className='testing-1'>It can accommodate 200 people</h2>
                 <Link to="/dashboard/:mycalendar" className='booking-title-1'><h1>Book It Now</h1></Link> 
             </div>
            </div>
            <div className='col-2'>
             <div className='bg-2'>
-                <h1 className='booking-title-2'> The place for your celebration! </h1>
-                <h2>Her you can celebratte your birthday with a beautifule view.</h2>
-                <h2>It can accommodate 50 people</h2>
+                <h1 className='testing-1'> The place for your celebration! </h1>
+                <h2 className='testing-1'>Her you can celebratte your birthday with a beautifule view.</h2>
+                <h2 className='testing-1'>It can accommodate 50 people</h2>
                 <Link to="/dashboard/:mycalendar" className='booking-title-1'><h1>Book It Now</h1></Link>
             </div>
            </div>
